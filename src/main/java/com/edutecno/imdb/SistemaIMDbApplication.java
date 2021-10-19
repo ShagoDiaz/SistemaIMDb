b9e5dd1e74286f67839f5d703893d8d2ee9b235f
Eliecer Diaz <eliecerdiazrojas@gmail.com>
2021-10-19 12:32:16 -0300

    Bean commandLineRunner implemented for set roles

--- src/main/java/com/edutecno/imdb/SistemaIMDbApplication.java
+++ src/main/java/com/edutecno/imdb/SistemaIMDbApplication.java
@@ -1,7 +1,12 @@
 package com.edutecno.imdb;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.edutecno.imdb.dao.RoleRepository;
+import com.edutecno.imdb.model.Role;
 
 @SpringBootApplication
 public class SistemaIMDbApplication {
@@ -10,4 +15,16 @@ public class SistemaIMDbApplication {
 		SpringApplication.run(SistemaIMDbApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner loadData(RoleRepository roleRepo) {
+		return (args) -> {
+			Role userRole = new Role();
+			userRole.setName("ROLE_USER");
+			roleRepo.save(userRole);
+
+			Role adminRole = new Role();
+			adminRole.setName("ROLE_ADMIN");
+			roleRepo.save(adminRole);
+		};
+	}
 }
